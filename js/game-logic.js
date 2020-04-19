3e656d31bb3d0842b19f40b6c840c1d1307e8ff8
Ankit pal <palankit35@gmail.com>
2020-04-20 01:48:49 +0530

    Adding Game Logic

---
+++ js/game-logic.js
@@ -0,0 +1,37 @@
+// All code should be written in this file.
+/*jshint esversion:6 */
+
+
+/*Players move type */
+let playerOneMoveOneType =undefined;
+let playerOneMoveTwoType = undefined;
+let playerOneMoveThreeType = undefined;
+    playerOneMoveOneValue = undefined;
+    playerOneMoveTwoValue = undefined;
+    playerOneMoveThreeValue = undefined;
+
+ playerTwoMoveOneType = undefined;
+ playerTwoMoveTwoType = undefined;
+ playerTwoMoveThreeType = undefined;
+ playerTwoMoveOneValue = undefined;
+ playerTwoMoveTwoValue = undefined;
+ playerTwoMoveThreeValue = undefined;
+
+
+/*Functions for PLayer's move */
+
+const setPlayerMoves= (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+    
+};
+
+const getRoundWinner= () => {
+
+};
+
+const getGameWinner = () =>{
+
+};
+
+const setComputerMoves = () =>{
+
+};
