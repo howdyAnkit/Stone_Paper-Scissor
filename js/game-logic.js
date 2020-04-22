8542cc262f6e599f084c25837a6b5692ac153af2
Ankit pal <palankit35@gmail.com>
2020-04-22 20:55:13 +0530

    Enumerating Changes

--- js/game-logic.js
+++ js/game-logic.js
@@ -18,7 +18,8 @@ let playerOneMoveOneType,
 
 /*Functions for PLayer's move */
 
-const setPlayerMoves= (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType,
+                        moveTwoValue, moveThreeType, moveThreeValue) {
 
         // Writing Fail tests 
     if (!moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeType || !moveThreeValue){
@@ -64,35 +65,63 @@ function isValidMoveValue(moveValue) {
     return (moveValue >= 1) && (moveValue <=99);
 }
 
-const getRoundWinner= (roundNumber) => {
-
+function getRoundWinner(roundNumber) {
     switch(roundNumber){
         case 1:
             return getMoveWinner(playerOneMoveOneType,
                                 playerOneMoveOneValue,
                                 playerTwoMoveOneType,
                                 playerTwoMoveOneValue);
-        
         case 2:
-            return getRoundWinner(playerTwoMoveTwoType,
-                                playerTwoMoveTwoValue,
+            return getRoundWinner(playerOneMoveTwoType,
+                                playerOneMoveTwoValue,
                                 playerTwoMoveTwoType,
                                 playerTwoMoveTwoValue);
 
         case 3:
             return getRoundWinner(playerOneMoveThreeType,
                                 playerOneMoveThreeValue,
-                                playerTwoMoveTwoType,
-                                playerTwoMoveTwoValue);
-
+                                playerTwoMoveThreeType,
+                                playerTwoMoveThreeValue);
         default:
             return null;
     }
 }
 
-const getGameWinner = () =>{
+function getMoveWinner(playerOneMoveType, playerOneMoveValue, playerTwoMoveType, playerTwoMoveValue){
 
-};
+    if(!playerOneMoveType || !playerOneMoveValue || !playerTwoMoveType || !playerTwoMoveValue){
+        return null;
+    }
+    if(playerOneMoveType === playerTwoMoveType){
+        if(playerOneMoveValue > playerTwoMoveValue){
+            return 'Player One';
+        }else if(playerOneMoveValue < playerTwoMoveValue){
+            return 'Player Two';
+        }else{
+            return 'Tie';
+        }
+    }
+    if(playerOneMoveType === 'rock'){
+        if(playerTwoMoveType === 'scissors'){
+            return 'Player One';
+        }else{
+            return 'Player Two';
+        }
+    }else if(playerOneMoveType === 'paper'){
+        if(playerTwoMoveType === 'rock'){
+            return 'Player One';
+        }else{
+            return 'Player Two';
+        }
+    }else{
+        if(playerTwoMoveType === 'paper'){
+            return 'Player One';
+        }else{
+            return 'Player Two';
+        }
+    }
+}
 
 const setComputerMoves = () =>{
 
